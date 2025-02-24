56da27882e39a70371b75f1f6954ccc1431085fd
Krithik Srinivas <krithiksrinivas200@gmail.com>
2025-02-24 19:58:03 +0530

    imported the search module

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import axios from "axios";
+import searchNodePackages from "./search-extension";
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -18,43 +19,7 @@ export async function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "gui-package-manager.search-npm-package",
     async () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const package_name = await vscode.window.showInputBox({
-        prompt: "Enter an npm package name",
-        placeHolder: "e.g., express, react, mongoose",
-      });
-      if (!package_name) {
-        return;
-      }
-      try {
-       console.log("package_name",package_name);
-        const res = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${package_name}&size=20`);
-        const data = await res.data
-		// console.log(data,"ajfljlkj");
-
-        if (!data.objects || data.objects.length === 0) {
-          vscode.window.showInformationMessage("No packages found.");
-          return;
-        }
-
-        const packageList = data.objects.map((pkg: any) => ({
-          label: pkg.package.name,
-          description: pkg.package.description || "No description available",
-          detail: `Version: ${pkg.package.version} | Author: ${
-            pkg.package.publisher.username || "Unknown"
-          }`,
-        }));
-		console.log(packageList,"pklist");
-        const selectedPackage = await vscode.window.showQuickPick(packageList, {
-          placeHolder: "Select an NPM package",
-        });
-      } catch (error) {
-        vscode.window.showErrorMessage(
-          "Error fetching data from npm registry."
-        );
-        console.error(error);
-      }
+      searchNodePackages();
     }
   );
 
